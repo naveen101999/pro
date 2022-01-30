8edcae170307205a3bd1cff07f0a149a8a2ffcd4
naveen101999 <93638367+naveen101999@users.noreply.github.com>
2022-01-30 22:42:28 +0530

    Update creating db and ec2.tf

--- creating db and ec2.tf
+++ creating db and ec2.tf
@@ -22,7 +22,7 @@ resource "aws_db_instance" "default" {
   name                 = "cpms"
   identifier           = "myrdb"
   username             = "admin"
-  password             = "Nareshkumar"
+  password             = "Naveen1999"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   publicly_accessible  = true
